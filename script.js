b1471d3c0dfd0cb43f652d1c32cd2eb900a316d3
Giorgio Andrei <cipuandrei@libero.it>
2021-07-15 12:04:48 +0200

    -

--- script.js
+++ script.js
@@ -11,16 +11,11 @@ let choosenDifficulty;
 
 let totalNumbers;
 
-let j = 0;//^USER NUMBER COUNTER AND ALSO SCORE COUNTER.
+let userScore = 0;//^USER NUMBER COUNTER AND ALSO SCORE COUNTER.
 
 function getRandomNumber(min, max) {//?RANDOM NUMBER GENERATOR.
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-
-
-
-
 
 //=GAME SETTINGS.
 do {
@@ -35,11 +30,11 @@ if (choosenDifficulty == difficulties[0]) {
     totalNumbers = 80;
     bombsNumber = 20;
 } else if (choosenDifficulty == difficulties[2]) {
-    totalNumbers = 50;
+    totalNumbers = 60;
     bombsNumber = 25;
 } else if (choosenDifficulty == difficulties[3]) {
-    totalNumbers = 35;
-    bombsNumber = 30;
+    totalNumbers = 30;
+    bombsNumber = 28;
 }
 
 //?BOMBS GENERATOR.
@@ -59,19 +54,20 @@ do {
         userNumbersList.push(chosenUserNumber);
     } else {
         alert('ATTENZIONE!! Inserisci un valore valido.');
-        j--;//^ COSI IL CONTATORE NON AUMENTA; SENNO' PER VINCERE BASTEREBBE INSERIRE
+        userScore--;//^ COSI IL CONTATORE NON AUMENTA; SENNO' PER VINCERE BASTEREBBE INSERIRE
         //^ SEMPRE LO STESSO NUMERO CONSAPEVOLMENTE.
     }
 
-    j++;
-} while (j < (totalNumbers - bombsNumber) && !(bombs.includes(chosenUserNumber)));
-
-if (bombs.includes(chosenUserNumber)) {
-    alert('Oh no! Hai preso una bomba ed hai perso.')
-}
+    userScore++;
+} while (userScore < (totalNumbers - bombsNumber) && !(bombs.includes(chosenUserNumber)));
 
 //?RESULTS.
-console.log("Numeri inseriti dall'utente: " + userNumbersList);
-console.log('Numeri delle bombe: ' + bombs.sort());
-console.log('Il numero che ti ha fatto perdere è: ' + chosenUserNumber);
-console.log('Il punteggio finale è di: ' + (j - 1) + '/' + (totalNumbers - bombsNumber) + ' punti.');
+if (bombs.includes(chosenUserNumber)) {
+    alert('Oh no! Hai preso una bomba ed hai perso.');
+    console.log("Numeri inseriti dall'utente: " + userNumbersList);
+    console.log('Numeri delle bombe: ' + bombs.sort());
+    console.log('Il numero che ti ha fatto perdere è: ' + chosenUserNumber);
+    console.log('Il punteggio finale è di: ' + (userScore - 1) + '/' + (totalNumbers - bombsNumber) + ' punti.');
+} else {
+    console.log('Congratulazioni! Hai vinto!')
+}
